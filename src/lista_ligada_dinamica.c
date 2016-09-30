f529044c31fe48445ec19f46ca35e1b5f80ef71e
Fernando Silva <liquuid@gmail.com>
2016-09-30 08:24:58 -0300

    insert ordenado funcionando

--- src/lista_ligada_dinamica.c
+++ src/lista_ligada_dinamica.c
@@ -42,64 +42,65 @@ NO* ultimoElemLista(LISTA *l)
     return(no);
 }
 
-NO* buscaSeqOrd(LISTA l, TIPOCHAVE ch)
-{
-    NO *no;
-    no = l.inicio;
-    if (no) {
-        while (no->prox && no->chave != ch ) {
-            no = no->prox;
-        }
-    }
-    return(no);
-}
 
+NO* buscaSeqOrd(LISTA *l, TIPOCHAVE ch, NO* *ant){
+    *ant = NULL;
+    NO* atual = l->inicio;
+    while ((atual != NULL) && (atual->chave < ch)){
+        *ant = atual;
+        atual = atual->prox;
+    }
+}
 
 bool inserirElemListaOrd(LISTA *l, TIPOCHAVE chave)
 {
     NO* p = (NO*) malloc(sizeof(NO));
-    NO* noTemp;
-    NO* anterior;
+    NO* noTemp = (NO*) malloc(sizeof(NO));
+    NO* anterior = (NO*) malloc(sizeof(NO));
+    NO* atual = (NO*) malloc(sizeof(NO));
+
+    p->chave = 0;
+    p->info = 0;
+    p->prox = NULL;
+
+    noTemp->chave = 0;
+    noTemp->info = 0;
+    noTemp->prox = NULL;
+
+    anterior->chave = 0;
+    anterior->info = 0;
+    anterior->prox = NULL;
+
+    atual->chave = 0;
+    atual->info = 0;
+    atual->prox = NULL;
+
     if ( l->inicio )
     {
-        /*if (tamanhoLista(l) == 1)
-        {
-            if(chave > l->inicio->chave)
-            {
+        if(tamanhoLista(*l) > 1){
+            buscaSeqOrd(l, chave, &anterior);
+            p->chave = chave;
+            p->prox = NULL;
+            if(anterior) {
+                p->prox = anterior->prox ;
+                anterior->prox = p;
+            } else {
+                p->prox = l->inicio;
+                l->inicio = p;
+            }
+
+        } else {
+            if ( l->inicio->chave > chave ){
+                p->chave = chave;
+                p->prox = l->inicio;
+                l->inicio = p;
+
+            } else {
+                l->inicio->prox = p;
                 p->chave = chave;
                 p->prox = NULL;
-                l->inicio->prox = p;
-            }
-            else
-            {
-                noTemp = l->inicio;
-                l->inicio = p;
-                p->chave = chave;
-                p->prox = noTemp;
             }
         }
-        else
-        {
-            noTemp = l->inicio;
-            while(noTemp && noTemp->chave < chave)
-            {
-                anterior = noTemp;
-                noTemp = noTemp->prox;
-            }
-            anterior->prox = p;
-            p->prox = noTemp;
-
-        }
-        /*
-        anterior = l->inicio;
-        while(anterior->prox && anterior->chave < chave)
-        {
-            anterior = anterior->prox;
-        }
-        anterior->prox
-        noTemp = buscaSeqOrd(*l, chave);
-        noTemp->prox = p; */
-
     }
     else
     {
@@ -152,13 +153,7 @@ int tamanhoLista(LISTA l)
     return(count);
 }
 
-
 /*
-
-
-
-
-
 void anexarElemLista()
 bool excluirElemLista()
 void destruirLista()
@@ -170,20 +165,25 @@ int main()
     inicializarLista(lista);
     printf("tamanho %d \n", tamanhoLista(*lista));
     inserirElemListaOrd(lista, 3);
-    /*inserirElemListaOrd(lista, 2);
+    inserirElemListaOrd(lista, 2);
     inserirElemListaOrd(lista, 4);
     inserirElemListaOrd(lista, 1);
+    inserirElemListaOrd(lista, 50);
+    inserirElemListaOrd(lista, 81);
+    inserirElemListaOrd(lista, 5);
+    inserirElemListaOrd(lista, 1);
     exibirLista(*lista);
-    printf("\nn = %d \n", enesimoElemLista(*lista,2)->chave );
-    printf("\nn = %d \n", enesimoElemLista(*lista,0)->chave );
-    printf("tamanho %d \n", tamanhoLista(*lista));
+    printf("n = %d \n", enesimoElemLista(*lista,2)->chave );
+    printf("n = %d \n", enesimoElemLista(*lista,0)->chave );
+
     inserirElemListaOrd(lista, 14);
     inserirElemListaOrd(lista, 52);
     inserirElemListaOrd(lista, 35);
     inserirElemListaOrd(lista, 47);
-    printf("tamanho %d \n", tamanhoLista(*lista));
-    exibirLista(*lista);*/
+    exibirLista(*lista);
+
 
     printf("\n");
+    printf("tamanho %d \n", tamanhoLista(*lista));
     return 0;
 }
